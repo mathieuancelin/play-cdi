983483e2f8ebb1bc3433748f26011432d23a35df
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-05-26 22:49:08 +0200

    Add support for generic types injection

--- src/play/inject/Injector2.java
+++ src/play/inject/Injector2.java
@@ -4,13 +4,17 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+import javax.enterprise.util.TypeLiteral;
 import javax.inject.Inject;
-import play.Play;
+import javax.inject.Qualifier;
 import play.classloading.enhancers.ControllersEnhancer.ControllerSupport;
 import play.jobs.Job;
 import play.mvc.Mailer;
+import play.Play;
 
 /**
  *
@@ -34,10 +38,21 @@ public class Injector2 {
                     Annotation[][] parameterAnnotations = method.getParameterAnnotations();
                     Object[] parameters = new Object[parameterTypes.length];
                     for (int j = 0; j < parameterTypes.length; j++) {
-                        parameters[j] = source.getBeanOfType(
-                                parameterTypes[j],
-                                //genericParameterTypes[j],
-                                parameterAnnotations[j]);
+                        Object value = null;
+                        if (genericParameterTypes[j] instanceof ParameterizedType) {
+                            try {
+                                value = source.getBeanOfType(
+                                        new ProgrammaticTypeLiteral(genericParameterTypes[j]),
+                                        getQualifiers(parameterAnnotations[j]));
+                            } catch (Throwable t) {
+                                t.printStackTrace();
+                            }
+                        }
+                        if (value == null) {
+                            value = source.getBeanOfType(parameterTypes[j],
+                                    getQualifiers(parameterAnnotations[j]));
+                        }
+                        parameters[j] = value;
                     }
                     boolean accessible = method.isAccessible();
                     // set a private method as public method to invoke it
@@ -62,15 +77,53 @@ public class Injector2 {
             for(Field field : clazz.getDeclaredFields()) {
                 if(Modifier.isStatic(field.getModifiers()) && field.isAnnotationPresent(Inject.class)) {
                     Class<?> type = field.getType();
+                    Type genericType = field.getGenericType();
+                    Object value = null;
+                    if (genericType instanceof ParameterizedType) {
+                        try {
+                            value = source.getBeanOfType(new ProgrammaticTypeLiteral(genericType),
+                                    getQualifiers(field.getAnnotations()));
+                        } catch (Throwable t) {
+                            t.printStackTrace();
+                        }
+                    }
                     field.setAccessible(true);
                     try {
-                        field.set(null, source.getBeanOfType(type));
+                        if (value == null) {
+                            value = source.getBeanOfType(type, getQualifiers(field.getAnnotations()));
+                        }
+                        field.set(null, value);
                     } catch(RuntimeException e) {
                         throw e;
                     } catch(Exception e) {
                         throw new RuntimeException(e);
                     }
                 }
+            }
+        }
+    }
+
+    private static Annotation[] getQualifiers(Annotation[] annotations) {
+        List<Annotation> qualifiers = new ArrayList<Annotation>();
+        for (Annotation annotation : annotations) {
+            if (annotation.annotationType().isAnnotationPresent(Qualifier.class)) {
+                qualifiers.add(annotation);
+            }
+        }
+        return qualifiers.toArray(new Annotation[qualifiers.size()]);
+    }
+
+    private static class ProgrammaticTypeLiteral extends TypeLiteral {
+
+        public ProgrammaticTypeLiteral(Type type) {
+            super();
+            try {
+                Field f = TypeLiteral.class.getDeclaredField("actualType");
+                f.setAccessible(true);
+                f.set(this, type);
+                f.setAccessible(false);
+            } catch (Exception ex) {
+                throw new RuntimeException("Can't create TypeLiteral for " + type, ex);
             }
         }
     }
